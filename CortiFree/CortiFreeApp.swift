66e4f0635c04c96b4b5b8e75e77ca47f2fd09c59
Josselin Biot <josselin.biot95@icloud.com>
2025-09-25 21:50:58 +0200

    Initial Commit

---
+++ CortiFree/CortiFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CortiFreeApp.swift
+//  CortiFree
+//
+//  Created by Josselin Biot on 25/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CortiFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
